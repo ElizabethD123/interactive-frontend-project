197c229eb5742b7df3d4c5cd690c1d1fcc4dc8cc
Ubuntu <ubuntu@ip-172-31-22-195.ec2.internal>
2019-09-17 12:16:30 +0000

    updating JS

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const cards = document.querySelectorAll('.game-card');
 
-function flipCard(){
-    this.classList.toggle('flip');
+let hasTurnedOverCard = false;
+let cardOne, cardTwo;
+
+function flipCard() {
+  this.classList.add('flip');
+
+  if (!hasTurnedOverCard) {
+    // first click
+    hasTurnedOverCard = true;
+    cardOne = this;
+
+    return;
+  }
+
+  // second click
+  hasTurnedOverCard = false;
+  cardTwo = this;
+
+  matchChecking();
+}
+
+function matchChecking() {
+  let isMatch = cardOne.dataset.name === cardTwo.dataset.name;
+
+  isMatch ? matchFound() : noMatch();
+}
+
+function matchFound() {
+  cardOne.removeEventListener('click', flipCard);
+  cardTwo.removeEventListener('click', flipCard);
+}
+
+function noMatch() {
+  setTimeout(() => {
+    cardOne.classList.remove('flip');
+    cardTwo.classList.remove('flip');
+  }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+  
