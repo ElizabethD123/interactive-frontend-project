2457af171945b3d3696621ac7a9ebe91d12b5a1c
Ubuntu <ubuntu@ip-172-31-22-195.ec2.internal>
2019-09-30 10:56:46 +0000

    major changes, removed some game cards due to complexity, updated css and restarted JS

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
 const cards = document.querySelectorAll('.game-card');
 
-let hasTurnedOverCard = false;
-let cardOne, cardTwo;
-
-function flipCard() {
-  this.classList.add('flip');
-
-  if (!hasTurnedOverCard) {
-    // first click
-    hasTurnedOverCard = true;
-    cardOne = this;
-
-    return;
-  }
-
-  // second click
-  hasTurnedOverCard = false;
-  cardTwo = this;
-
-  matchChecking();
+function cardFlip() {
+  this.classList.toggle('flip');
 }
 
-function matchChecking() {
-  let isMatch = cardOne.dataset.name === cardTwo.dataset.name;
-
-  isMatch ? matchFound() : noMatch();
-}
-
-function matchFound() {
-  cardOne.removeEventListener('click', flipCard);
-  cardTwo.removeEventListener('click', flipCard);
-}
-
-function noMatch() {
-  setTimeout(() => {
-    cardOne.classList.remove('flip');
-    cardTwo.classList.remove('flip');
-  }, 1500);
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-  
+cards.forEach(card => card.addEventListener('click', cardFlip));
