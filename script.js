ea6cbe5e90422941e0ffc61895cb188a30805767
Ubuntu <ubuntu@ip-172-31-22-195.ec2.internal>
2019-09-30 11:19:37 +0000

    changed and updated Javasctipt

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const cards = document.querySelectorAll('.game-card');
 
+let turnedOverCard = false;
+let cardOne, cardTwo;
+
+
+
 function cardFlip() {
-  this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!turnedOverCard) {
+
+        turnedOverCard = true;
+        cardOne = this;
+    }
+    else {
+
+        turnedOverCard = false;
+        cardTwo = this;
+
+        // card match
+
+        if (cardOne.dataset.name === cardTwo.dataset.name) {
+
+            cardOne.removeEventListener('click', cardFlip);
+            cardTwo.removeEventListener('click', cardFlip);
+
+        }
+        else {
+
+            setTimeout(() => {
+                cardOne.classList.remove('flip');
+                cardTwo.classList.remove('flip');
+            }, 1500);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', cardFlip));
