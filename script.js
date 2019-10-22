5f5a90d77aeca967698bcf7bc9e78e0815d250ce
Ubuntu <ubuntu@ip-172-31-22-195.ec2.internal>
2019-10-22 09:44:28 +0000

    changing css to help smoother styling

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const cards = document.querySelectorAll('.game-card');
 let turnedOverCard = false;
 let lockBoard = false;
 let cardOne, cardTwo;
+let matchedCards = 0;
 
 
 
 function cardFlip() {
-    if (lockBoard) return; 
-    if (this === cardOne) return; 
-    
+    if (lockBoard) return;
+    if (this === cardOne) return;
+
     this.classList.add('flip');
 
     if (!turnedOverCard) {
@@ -21,56 +22,59 @@ function cardFlip() {
 
         turnedOverCard = false;
         cardTwo = this;
-        
-    matchChecking();
-        
+
+        matchChecking();
+
     }
 
+}
+
+function matchChecking() {
+
+    let foundMatch = cardOne.dataset.name === cardTwo.dataset.name;
+    foundMatch ? disableCard() : flipBackCard();
+    
+   matchedCards = foundMatch ? matchedCards +2: matchedCards;
+   
+   if (matchedCards === 12) {
+        console.log(matchedCards);
+   }
 
 
 }
 
-function matchChecking () {
-    
-        let foundMatch = cardOne.dataset.name === cardTwo.dataset.name ; 
-foundMatch ? disableCard() : flipBackCard(); 
-
-
-    }
-    
 function disableCard() {
-      cardOne.removeEventListener('click', cardFlip);
-            cardTwo.removeEventListener('click', cardFlip);
+    cardOne.removeEventListener('click', cardFlip);
+    cardTwo.removeEventListener('click', cardFlip);
+
+    boardReset();
 
 }
 
 function flipBackCard() {
-    
+
     lockBoard = true;
-    
-    
-    
-            setTimeout(() => {
-                cardOne.classList.remove('flip');
-                cardTwo.classList.remove('flip');
-                
-                boardReset();
-                
-                
-            }, 500);
-        }
+
+
+
+    setTimeout(() => {
+        cardOne.classList.remove('flip');
+        cardTwo.classList.remove('flip');
+        boardReset();
+    }, 500);
+}
 
 function boardReset() {
-    
+
     [turnedOverCard, lockBoard] = [false, false];
     [cardOne, cardTwo] = [null, null];
 }
 
 (function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
 })();
 
 
